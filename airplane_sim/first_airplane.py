f75fa3d569b5f4642401b5be85504bd63012aec7
Jonathon-E <joe5721@utulsa.edu>
2024-01-19 11:54:11 -0600

    Added class

---
+++ airplane_sim/first_airplane.py
@@ -0,0 +1,3 @@
+class airplane_jme:
+    def __init__(self):
+        print("w")
--- start.py
+++ start.py
@@ -2,11 +2,13 @@ import numpy as np
 import matplotlib as mpl
 from matplotlib import pyplot as plt
 
+from airplane_sim.first_airplane import airplane_jme
+
+Air = airplane_jme()
+print("Hello Connor")
+
 xpoints = np.array([0, 6])
 ypoints = np.array([0, 250])
 
 plt.plot(xpoints, ypoints)
 plt.show()
-
-
-print("Hello Connor")
