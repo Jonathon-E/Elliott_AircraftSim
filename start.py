287430b5b96cde454f07d3128f37904b9abdfd9a
Jonathon-E <joe5721@utulsa.edu>
2024-01-19 11:36:00 -0600

    start

---
+++ start.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
+
+
+print("Hello Connor")
